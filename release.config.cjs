fe44c03cc089efbba7c5977eb56bb2a5be841e99
David Sveningsson <ext@sidvind.com>
2025-04-30 21:44:06 +0200

    ci: add proper release config

---
+++ release.config.cjs
@@ -0,0 +1,19 @@
+const commitAnalyzer = require("@html-validate/semantic-release-config/lib/commit-analyzer");
+const releaseNotesGenerator = require("@html-validate/semantic-release-config/lib/release-notes-generator");
+const npm = require("@html-validate/semantic-release-config/lib/npm");
+const changelog = require("@html-validate/semantic-release-config/lib/changelog");
+const exec = require("@html-validate/semantic-release-config/lib/exec");
+const git = require("@html-validate/semantic-release-config/lib/git");
+const branches = require("@html-validate/semantic-release-config/lib/branches");
+
+const plugins = [
+	["@semantic-release/commit-analyzer", commitAnalyzer],
+	["@semantic-release/release-notes-generator", releaseNotesGenerator],
+	["@semantic-release/npm", npm],
+	["@semantic-release/github", {}],
+	["@semantic-release/changelog", changelog],
+	["@semantic-release/exec", exec],
+	["@semantic-release/git", git],
+];
+
+module.exports = { branches, plugins };
